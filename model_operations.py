9732a46a23d05b02cc7bdd650ae4147632ef5f74
nsunda205 <nagaraj_sundaramahalingam@comcast.com>
2024-03-13 16:50:39 +0530

    clean up

--- model_operations.py
+++ model_operations.py
@@ -1,44 +1,9 @@
 import requests
-# import novel_details
 import utils
 import configurations as config
 
 
-# system_prompt = utils.get_system_prompt(novel_details.novel_inputs)
-# print(system_prompt)
-
-
-# url = f'{config.GPT_URL}{config.GPT_USAGE_ENDPOINT}'
-
-# prompt = 'Give me the chapter details. Total chapter is 20'
-
-# headers = {
-#     'Authorization': f'Bearer {config.TOKEN}',
-#     'Content-Type': 'application/json'
-# }
-
-# data = {'messages': [
-#     {'role': 'system', 'content': system_prompt},
-#     {'role': 'user', 'content': prompt}
-# ],
-#     'model_id': config.GPT35_TURBO_MODEL,
-#     'temperature': 0.7}
-
-
-# response = requests.post(url, headers=headers, json=data)
-
-# output = None
-
-# if response.ok:
-#     response = response.json()
-#     first_choice = response.get('choices')[0]
-#     output = first_choice.get('message', {}).get('content')
-
-# if output:
-#     print(output)
-
-
-def get_output_from_model(system_prompt, user_prompt):
+def get_output_from_model(system_prompt, user_prompt, temperature=0.9):
     url = f'{config.GPT_URL}{config.GPT_USAGE_ENDPOINT}'
 
     headers = {
@@ -51,7 +16,7 @@ def get_output_from_model(system_prompt, user_prompt):
         {'role': 'user', 'content': user_prompt}
     ],
         'model_id': config.GPT35_TURBO_MODEL,
-        'temperature': 0.7}
+        'temperature': temperature}
 
     response = requests.post(url, headers=headers, json=data)
 

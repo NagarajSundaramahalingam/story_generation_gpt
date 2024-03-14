222790236758e51a9e1c5e13075b6ae958d66cc3
nsunda205 <nagaraj_sundaramahalingam@comcast.com>
2024-03-14 16:46:27 +0530

    pdf save

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import configurations as config
 from jinja2 import Environment, FileSystemLoader
-from reportlab.pdfgen import canvas
+from fpdf import FPDF
 
 
 def get_system_prompt(novel_details):
@@ -13,6 +13,11 @@ def get_system_prompt(novel_details):
 
 
 def text_to_pdf(content, filepath):
-    pdf_canvas = canvas.Canvas(filepath)
-    pdf_canvas.drawString(100, 750, content)
-    pdf_canvas.save()
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('Arial', size=13)
+    encoder = 'latin-1'
+    content = content.encode('latin-1', errors='ignore')
+    content = content.decode()
+    pdf.multi_cell(0, 10, content)
+    pdf.output(filepath)

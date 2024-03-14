e1b841f91c12f9e78e2d6846b02f018397e74622
nsunda205 <nagaraj_sundaramahalingam@comcast.com>
2024-03-14 15:56:20 +0530

    more config

--- configurations.py
+++ configurations.py
@@ -9,7 +9,8 @@ GPT35_TURBO_MODEL = 'Databricks_GPT35_Turbo_16k'
 TEMPLATES_FOLDER = 'templates/'
 SYSTEM_PROMPT_TEMPLATE_FILE = 'system_prompt.j2'
 
-GENRES = ['Sci-Fi', 'Fantasy', 'Mystery', 'Romance']
+GENRES = ['Action', 'Adventure', 'Crime', 'Dark', 'Drama',
+          'Fantasy', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller']
 
 NARRATIVE_STYLE = ['First-Person', 'Second-Person', 'Third-Person']
 
